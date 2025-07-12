79c8bde225d3034b196c91779cb1844273f48312
Surabhi-M-R <surabhimr27@gmail.com>
2025-07-12 22:25:05 +0530

    environment variable creation

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+
 // creating an instance 
 const app=express();
 
@@ -7,7 +8,7 @@ app.get("/about",(req,res)=>res.send(" hello about page  ") );
 app.get("/contact",(req,res)=>res.send(" hello contact page ") );
 app.get("/last",(req,res)=>res.send(" hello last page ") );
 
-const PORT=3000;
+const PORT =process.env.PORT ||3000 ;
 app.listen(PORT,()=>{
     console.log(`Server is running at PORT : ${PORT}`);
 
