25241b08c06e59a7a17495b46cfe0b5ac5758370
Surabhi-M-R <surabhimr27@gmail.com>
2025-07-16 22:49:45 +0530

    zod commit

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import { PORT } from './env.js';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-// ✅ Convert import.meta.url to __filename and __dirname
+// Convert import.meta.url to __filename and __dirname
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -13,6 +13,13 @@ const app = express();
 console.log("Outside route:");
 console.log("__filename:", __filename);
 console.log("__dirname:", __dirname);
+
+const response=await fetch("https://jsonplaceholder.typicode.com/todos/1");
+const json=await response.json();
+console.log(json);
+
+// In newer version of node.js you can use top-level await without needing to wrap it in an async function.
+
 
 
 const staticPath=path.join(import.meta.dirname,"public","index.html");
@@ -23,7 +30,7 @@ app.get("/", (req, res) => {
     res.sendFile(staticPath);
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, () => { 
     console.log(`Server is running at PORT: ${PORT}`);
 });
 
