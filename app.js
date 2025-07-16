5f0196eceb39fc4f612d8a90c906caf85aaedefa
Surabhi-M-R <surabhimr27@gmail.com>
2025-07-16 16:43:23 +0530

    zod commit

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import {PORT} from './env.js';
 
 // creating an instance 
 const app=express();
@@ -8,7 +9,7 @@ app.get("/about",(req,res)=>res.send(" hello about page  ") );
 app.get("/contact",(req,res)=>res.send(" hello contact page ") );
 app.get("/last",(req,res)=>res.send(" hello last page ") );
 
-const PORT =process.env.PORT ||3000 ;
+//const PORT =process.env.PORT ||3000 ;
 app.listen(PORT,()=>{
     console.log(`Server is running at PORT : ${PORT}`);
 
---
+++ env.js
@@ -0,0 +1,3 @@
+//  export const PORT =isNaN(process.env.PORT)? 3000 :parseInt(process.env.PORT);
+
+import {z} from 'zod';
