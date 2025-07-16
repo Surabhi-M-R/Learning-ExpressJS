67bb30d254650087bd89b0981411d3baca687a45
Surabhi-M-R <surabhimr27@gmail.com>
2025-07-16 17:19:25 +0530

    for zod

--- env.js
+++ env.js
@@ -1,3 +1,9 @@
 //  export const PORT =isNaN(process.env.PORT)? 3000 :parseInt(process.env.PORT);
 
 import {z} from 'zod';
+
+const ageSchema=z.number().min(18).max(100).int();
+const userAge=17;
+
+const parseUserAge=ageSchema.parse(userAge);
+console.log(parseUserAge);
