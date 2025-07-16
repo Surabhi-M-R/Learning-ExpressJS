ed572419f407a9c7559cdecffc5f30f41b2b2cff
Surabhi-M-R <surabhimr27@gmail.com>
2025-07-16 17:30:27 +0530

    zod final

--- env.js
+++ env.js
@@ -1,9 +1,15 @@
-//  export const PORT =isNaN(process.env.PORT)? 3000 :parseInt(process.env.PORT);
+// //  export const PORT =isNaN(process.env.PORT)? 3000 :parseInt(process.env.PORT);
 
-import {z} from 'zod';
+// import {z} from 'zod';
 
-const ageSchema=z.number().min(18).max(100).int();
-const userAge=17;
+// const ageSchema=z.number().min(18).max(100).int();
+// const userAge=17;
 
-const parseUserAge=ageSchema.parse(userAge);
-console.log(parseUserAge);
+// const parseUserAge=ageSchema.parse(userAge);
+// console.log(parseUserAge);
+
+
+import { z} from 'zod';
+
+export const portSchema = z.coerce.number().min(1).max(65535).default(3000); // the coerce method is used to convert string to number if it is in the double quotes
+export const PORT =portSchema.parse(process.env.PORT);
