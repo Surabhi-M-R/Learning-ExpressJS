765033cbdb80d68c805046f1007eb39be7883961
Surabhi-M-R <surabhimr27@gmail.com>
2025-07-20 12:55:47 +0530

    mongoDB crud operation

--- mongodb/mongo-driver.js
+++ mongodb/mongo-driver.js
@@ -8,9 +8,50 @@ await client.connect(); // then connect
 const db= client.db("mongodb_nodejs_db");
 const userCollection=db.collection("users");
 
-userCollection.insertOne({ name:"sush", 
-    age:21,
-    course:"data science",
-    degree:" Bachelors of engineering "
-})
+// userCollection.insertOne({ name:"sush", 
+//     age:21,
+//     course:"data science",
+//     degree:" Bachelors of engineering "
+// })
 
+// userCollection.insertMany([
+//     { name:"sush", 
+//     age:51,
+//     course:"ai and ml",
+//     degree:" Bachelors of engineering "
+// },
+// { name:"sush", 
+//     age:21,
+//     course:"data science",
+//     degree:" Bachelors of engineering "
+// },
+// { name:"sush", 
+//     age:24,
+//     course:"computer science and engineering ",
+//     degree:" Bachelors of engineering "
+// }
+// ]);
+
+
+// Read operation 
+// const usersCursor=userCollection.find();
+// console.log(usersCursor);
+
+// for  await (const user of usersCursor){
+//     console.log(user);
+// }
+// const usersCursor=await userCollection.find().toArray();
+// console.log(usersCursor);
+
+// Find one colletion
+
+// const users= await userCollection.findOne({ age:51});
+// console.log(users.name);
+// console.log(users._id.toHexString());
+
+// update 
+//await userCollection.updateOne({course:"ai and ml"},{$set:{age:22}});
+
+// delete
+
+await userCollection.deleteOne({course:"ai and ml"});
